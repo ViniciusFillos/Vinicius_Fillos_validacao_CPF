80231c84a8673c72b7f88350b8cfa05ed8678c63
Vinicius Fillos <vinifillos@gmail.com>
2024-04-10 11:22:32 -0300

    feat: método validacaoNumerico adicionado

--- src/main/java/org/compass/servicos/Valicao.java
+++ src/main/java/org/compass/servicos/Valicao.java
@@ -1,8 +1,21 @@
 package org.compass.servicos;
 
+import org.compass.entidades.CPF;
+
 public class Valicao {
 
-    public static boolean validarTamanho(String cpf){
-            return cpf.length()==11;
+    public static boolean validarTamanho(CPF cpf){
+        return cpf.getCpf().length()==11;
+    }
+
+    public static boolean validarNumerico(CPF cpf){
+        boolean flag;
+        try {
+            long iscpf = Long.parseLong(cpf.getCpf());
+            flag = true;
+        } catch (Exception e){
+            flag = false;
+        }
+        return flag;
     }
 }
